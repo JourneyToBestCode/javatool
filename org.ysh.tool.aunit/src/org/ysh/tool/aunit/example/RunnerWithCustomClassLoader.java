47d6de79a9ba6a46b45fc1f0d3f7c8bcbd7f6752
yangshuo <yangshuosdu@gmail.com>
2012-12-14 13:39:10 +0800

    Remove some realpath on my laptob

--- org.ysh.tool.aunit/src/org/ysh/tool/aunit/example/RunnerWithCustomClassLoader.java
+++ org.ysh.tool.aunit/src/org/ysh/tool/aunit/example/RunnerWithCustomClassLoader.java
@@ -10,7 +10,8 @@ public class RunnerWithCustomClassLoader extends RunnerWithDefaultClassLoader{
 	}
 	
 	public static void main(String[] args) throws Throwable{
-		System.setProperty("myClassPath", "C:\\Users\\eshoyag\\workspace\\org.ysh.tool.aunit\\bin\\org\\ysh\\tool\\aunit\\example");
+		//Note: Pls change the value of myClassPath to the path in your own labtop
+		System.setProperty("myClassPath", "mock");
 		Thread.currentThread().setContextClassLoader(new MyClassLoader());
 		System.out.println(RunnerWithCustomClassLoader.class.getClassLoader() == Thread.currentThread().getContextClassLoader());
 		new RunnerWithCustomClassLoader().run();
